eda6c814559f72be142eb2b0b20fdec118a349e7
Arnav Gattani <112846923+agattani123@users.noreply.github.com>
2025-07-22 03:29:11 -0700

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,11 +1,11 @@
 import roles from '../config/roles.json' assert { type: 'json' };
 
-const db = {
-  users: {
-    'admin@internal.company': 'admin',
-    'analyst@internal.company': 'analyst',
-  },
-  roles: roles
-};
+// const db = {
+//   users: {
+//     'admin@internal.company': 'admin',
+//     'analyst@internal.company': 'analyst',
+//   },
+//   roles: roles
+// };
 
 export default db;
