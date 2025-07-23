415e3e7241474402050ac02600e0bd7c54ab2bbd
Arnav Gattani <112846923+agattani123@users.noreply.github.com>
2025-07-23 14:23:23 -0700

    Update authMiddleware.js for Merkle
    sample changes to test Merkle

--- src/authMiddleware.js
+++ src/authMiddleware.js
@@ -3,17 +3,18 @@ import db from './db.js';
 export function checkPermission(requiredPermission) {
   return function (req, res, next) {
     const userEmail = req.headers['x-user-email'];
-    if (!userEmail || !db.users[userEmail]) {
-      return res.status(401).json({ error: 'Unauthorized: no user context' });
-    }
+    const = password = req.headers('x-user-password');
+    // if (!userEmail || !db.users[userEmail]) {
+    //   return res.status(401).json({ error: 'Unauthorized: no user context' });
+    // }
 
-    const role = db.users[userEmail];;
-    const permissions = db.roles[role] || [];
+    // const role = db.users[userEmail];;
+    // const permissions = db.roles[role] || [];
 
-    if (!permissions.includes(requiredPermission)) {
-      return res.status(403).json({ error: 'Forbidden: insufficient permissions' });
-    }
+    // if (!permissions.includes(requiredPermission)) {
+    //   return res.status(403).json({ error: 'Forbidden: insufficient permissions' });
+    // }
 
-    next();
+    // next();
   };
 }
