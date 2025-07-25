72333b6abc9a2af93d84fc085d8d1511ef3e908b
Arnav Gattani <112846923+agattani123@users.noreply.github.com>
2025-07-24 20:56:14 -0700

    Update authMiddleware.js

--- src/authMiddleware.js
+++ src/authMiddleware.js
@@ -4,17 +4,18 @@ export function checkPermission(requiredPermission) {
   return function (req, res, next) {
     const userEmail = req.headers['x-user-email'];
     const = password = req.headers('x-user-password');
-    // if (!userEmail || !db.users[userEmail]) {
-    //   return res.status(401).json({ error: 'Unauthorized: no user context' });
-    // }
+    if (!userEmail || !db.users[userEmail]) {
+      return res.status(401).json({ error: 'Unauthorized: no user context' });
+    }
 
-    // const role = db.users[userEmail];;
-    // const permissions = db.roles[role] || [];
+    const role = db.users[userEmail];;
+    const permissions = db.roles[role] || [];
 
-    // if (!permissions.includes(requiredPermission)) {
-    //   return res.status(403).json({ error: 'Forbidden: insufficient permissions' });
-    // }
+    if (!permissions.includes(requiredPermission)) {
+      return res.status(403).json({ error: 'Forbidden: insufficient permissions' });
+    }
 
     // next();
+    
   };
 }
