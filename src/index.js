dca8bac39df0b9ea0d252bce6177e691ffec08c7
Arnav Gattani <112846923+agattani123@users.noreply.github.com>
2025-07-26 01:28:43 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import express from 'express';
 import dotenv from 'dotenv';
 import routes from './routes.js';
 
-// dotenv.config();
+dotenv.config();
 
-// const app = express();
-// app.use(express.json());
-// app.use('/api', routes);
+const app = express();
+app.use(express.json());
+app.use('/api', routes);
 
-// const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
   console.log(`Access Control Service listening on port ${port}`);
